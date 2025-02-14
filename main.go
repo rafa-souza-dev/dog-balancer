613e744e91979249b09c20349bafa0b6a6cdc012
Rafael Souza <rafaeljosegomes25@gmail.com>
2025-02-14 00:51:52 -0300

    feat: sharing mutex

--- main.go
+++ main.go
@@ -3,15 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 	"time"
+
+	reverseproxy "github.com/rafa-souza-dev/dog-balancer/reverse_proxy"
+)
+
+var (
+	backends = []string{
+		"http://localhost:8001",
+		"http://localhost:8002",
+		"http://localhost:8003",
+		"http://localhost:8004",
+	}
+
+	currentIndex = 0
 )
 
 func main() {
 	log.Println("Dog-balancer started!")
 	mux := http.NewServeMux()
+	mu := sync.Mutex{}
+
+	mux.HandleFunc("/", reverseproxy.HandleRedirectRequest(
+		&currentIndex,
+		&mu,
+		backends,
+	))
 
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: ":8000",
 		Handler: mux,
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 10 * time.Second,
