55dbecce88c7e7a20ae5dd7d841bfb103e1fcde9
Rafael Souza <rafaeljosegomes25@gmail.com>
2025-02-13 23:44:55 -0300

    feat: start dog-balancer server

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	log.Println("Dog-balancer started!")
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout: 1 * time.Minute,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalln(err)
+		panic(err)
+	}
+}
